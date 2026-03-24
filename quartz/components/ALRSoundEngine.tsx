f8cee4ec14957bc3117f3d498e5675f84cd0c2f9
Samurai-Lemon <lamouttejohn@gmail.com>
2026-03-23 22:37:38 -0400

    add ambient sound

--- quartz/components/ALRSoundEngine.tsx
+++ quartz/components/ALRSoundEngine.tsx
@@ -122,7 +122,7 @@ const ALRSoundEngine: QuartzComponent = () => {
                 source.loop = true;
 
                 const gain = ctx.createGain();
-                gain.gain.value = 0.005;
+                gain.gain.value = 0.020;
 
                 source.connect(gain);
                 gain.connect(ctx.destination);
